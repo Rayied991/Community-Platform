7d1d32cdfbe0ddc5ebc9a7e90317274bc3136eba
MD.SHAHZAD HUSSAIN RAYIED <rayiedhussain8@gmail.com>
2026-04-12 00:05:38 +0600

    shadcn  initialized

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
    <div>
     <h1>Hello Next.JS!</h1>
+    <Button size={"lg"}>Subscribe</Button>
    </div>
   );
 }
