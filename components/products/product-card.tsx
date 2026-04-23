81f4c0b180f55e2712d9509db27502ac60a27275
MD.SHAHZAD HUSSAIN RAYIED <rayiedhussain8@gmail.com>
2026-04-24 01:48:31 +0600

    upvote and downvote feature implemented

--- components/products/product-card.tsx
+++ components/products/product-card.tsx
@@ -1,11 +1,10 @@
 import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
 import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { products } from "@/db/schema";
-import { cn } from "@/lib/utils";
 import { InferSelectModel } from "drizzle-orm";
-import { ChevronDownIcon, ChevronUpIcon, StarIcon } from "lucide-react";
+import { StarIcon } from "lucide-react";
 import Link from "next/link";
+import VotingButtons from "./voting-buttons";
 type Product=InferSelectModel<typeof products>;
 const ProductCard = ({product}:{product:Product}) => {
     const hasVoted=false;
@@ -29,16 +28,7 @@ const ProductCard = ({product}:{product:Product}) => {
                  <CardDescription>{product.description}</CardDescription>
                 </div>
                 {/*  Voting buttons -> upvote or downvote */}
-                <div className="flex flex-col items-center gap-1 shrink-0">
-
-                    <Button variant={"ghost"} size={"icon-sm"} className={cn("h-8 w-8 text-primary ", hasVoted? "bg-primary/10 text-primary hover:bg-primary/20":"hover:bg-primary/10 hover:text-primary")}>
-                    <ChevronUpIcon className="size-5"/>
-                    </Button>
-                    <span className="text-sm font-semibold transition-colors text-foreground">{product.voteCount}</span>
-                     <Button variant={"ghost"} size={"icon-sm"} className={cn("h-8 w-8 text-primary ",hasVoted? "hover:text-destructive" : "opacity-50 cursor-not-allowed" )}>
-                    <ChevronDownIcon className="size-5"/>
-                    </Button>
-                </div>
+               <VotingButtons hasVoted={hasVoted} voteCount={product.voteCount}  productId={product.id}/>
                 </div>
                
             </CardHeader>
