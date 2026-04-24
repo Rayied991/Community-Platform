d7a6f95777129a743e14b79a05d7f274c38987d8
MD.SHAHZAD HUSSAIN RAYIED <rayiedhussain8@gmail.com>
2026-04-25 01:31:02 +0600

    Dynamic Product details page implemented

--- components/products/product-card.tsx
+++ components/products/product-card.tsx
@@ -9,7 +9,7 @@ type Product=InferSelectModel<typeof products>;
 const ProductCard = ({product}:{product:Product}) => {
     const hasVoted=false;
   return (
-    <Link href={`/products/${product.id}`}>
+    <Link href={`/products/${product.slug}`}>
 
         <Card className="group card-hover hover:bg-primary-foreground/10 border-solid border-gray-400 min-h-50">
             <CardHeader className="flex-1">
